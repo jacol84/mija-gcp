641dc628f152e09b8e92aedc629195145d895157
jac <devjac@ogarnij.se>
2021-05-01 19:19:52 +0200

    mija-1  init User Rest clean rest API

--- gpc-app/src/main/kotlin/pl/mija/gcp/app/user/User.kt
+++ gpc-app/src/main/kotlin/pl/mija/gcp/app/user/User.kt
@@ -1,3 +1,4 @@
 package pl.mija.gcp.app.user
 
+data class UserCreate(val name: String, val lastName: String)
 data class User(val id: Long, val name: String, val lastName: String)
--- gpc-app/src/main/kotlin/pl/mija/gcp/app/user/UserRest.kt
+++ gpc-app/src/main/kotlin/pl/mija/gcp/app/user/UserRest.kt
@@ -3,41 +3,47 @@ package pl.mija.gcp.app.user
 import com.google.gson.Gson
 import io.ktor.application.*
 import io.ktor.http.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
-import java.util.*
 import java.util.concurrent.atomic.AtomicLong
 
 val id = AtomicLong(0)
 
 
 fun userAPI(): Route.() -> Unit = {
-    accept(ContentType.Application.Json) {
-        get("/user") {
-            val result = UserController().getAll()
-            call.respondText(Gson().toJson(result), ContentType.Application.Json)
-        }
-        post("/user") {
-            TODO("read post parameter")
-            val create = UserController().create()
-            call.respond(create)
-        }
-        get("/user/{id}") {
-            val id = call.parameters["id"]!!.toLong()
-            val result = UserController().getById(id)
-            call.respondText(Gson().toJson(result), contentType = ContentType.Application.Json)
-        }
-        put("/user/{id}") {
-//            TODO()
-            val result = UserController().getAll()
-            call.respondText(Gson().toJson(result), contentType = ContentType.Application.Json)
-        }
-        delete("/user/{id}") {
-            val id = call.parameters["id"]!!.toLong()
-            val delete = UserController().delete(id)
-            call.respondText(Gson().toJson(delete), contentType = ContentType.Application.Json)
+//    accept(ContentType.Application.Json) {
+    get("/user") {
+        val result = UserController().getAll()
+        call.respond(HttpStatusCode.OK, result)
+    }
+    get("/user/{id}") {
+        val id = call.parameters["id"]!!.toLong()
+        val result = UserController().getById(id)
+        if (result != null) call.respond(result) else call.respond(HttpStatusCode.NotFound, Gson().toJson("No found user by id=$id"))
+
+    }
+    post("/user") {
+        val user = call.receive<UserCreate>()
+        val create = UserController().create(user)
+        call.respond(HttpStatusCode.Created, create)
+    }
+    put("/user/{id}") {
+        val id = call.parameters["id"]!!.toLong()
+        val user = call.receive<User>()
+        if (user.id == id) {
+            val result = UserController().put(id, user)
+            call.respond(HttpStatusCode.Created, result)
+        } else {
+            call.respond(HttpStatusCode.NotFound, Gson().toJson("No found user by id=$id"))
         }
     }
+    delete("/user/{id}") {
+        val id = call.parameters["id"]!!.toLong()
+        val delete = UserController().delete(id)
+        call.respondText(Gson().toJson(delete), contentType = ContentType.Application.Json)
+    }
+//    }
 }
 
 val mutableList = mutableListOf<User>()
@@ -51,15 +57,20 @@ class UserController {
         return mutableList.find { it.id == id }
     }
 
-    fun create(name: String = "Jacek", lastName: String = UUID.randomUUID().toString()): User {
-        val element = User(id.incrementAndGet(), name, lastName)
+    fun create(user: UserCreate): User {
+        val element = User(id.incrementAndGet(), user.name, user.lastName)
         mutableList.add(element)
         return element
+    }
+
+    fun put(id: Long, user: User): User {
+        mutableList.removeIf { it.id == id }
+        mutableList.add(user)
+        return user
     }
 
     fun delete(id: Long): Boolean {
         return mutableList.removeIf { it.id == id }
     }
-
 
 }
