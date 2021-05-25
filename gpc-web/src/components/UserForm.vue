c1194f0fe4d27b119f64365e5343a7e14f970f12
jac <devjac@ogarnij.se>
2021-05-25 20:56:54 +0200

    mija-1 add ant-design-vue tbc... fix bug to build

--- gpc-web/src/components/UserForm.vue
+++ gpc-web/src/components/UserForm.vue
@@ -1,37 +1,41 @@
 <template>
-    <a-form
-        layout="inline"
-        :model="formState"
-        @finish="handleFinish"
-        @finishFailed="handleFinishFailed"
-    >
-      <a-form-item>
-        <a-input v-model:value="formState.user" placeholder="Username">
-          <template #prefix><UserOutlined style="color: rgba(0, 0, 0, 0.25)" /></template>
-        </a-input>
-      </a-form-item>
-      <a-form-item>
-        <a-input v-model:value="formState.password" type="password" placeholder="Password">
-          <template #prefix><LockOutlined style="color: rgba(0, 0, 0, 0.25)" /></template>
-        </a-input>
-      </a-form-item>
-      <a-form-item>
-        <a-button
-            type="primary"
-            html-type="submit"
-            :disabled="formState.user === '' || formState.password === ''"
-        >
-          Log in
-        </a-button>
-      </a-form-item>
-    </a-form>
+  <a-form
+      layout="inline"
+      :model="formState"
+      @finish="handleFinish"
+      @finishFailed="handleFinishFailed"
+  >
+    <a-form-item>
+      <a-input v-model:value="formState.user" placeholder="Username">
+        <template #prefix>
+          <UserOutlined style="color: rgba(0, 0, 0, 0.25)"/>
+        </template>
+      </a-input>
+    </a-form-item>
+    <a-form-item>
+      <a-input v-model:value="formState.password" type="password" placeholder="Password">
+        <template #prefix>
+          <LockOutlined style="color: rgba(0, 0, 0, 0.25)"/>
+        </template>
+      </a-input>
+    </a-form-item>
+    <a-form-item>
+      <a-button
+          type="primary"
+          html-type="submit"
+          :disabled="formState.user === '' || formState.password === ''"
+      >
+        Log in
+      </a-button>
+    </a-form-item>
+  </a-form>
 </template>
 <script lang="ts">
 
 import {UserOutlined, LockOutlined} from '@ant-design/icons-vue';
 import {ValidateErrorEntity} from 'ant-design-vue/es/form/interface';
 import {defineComponent, reactive, UnwrapRef} from 'vue';
-import {Form,  Row} from 'ant-design-vue';
+import {Form, Row} from 'ant-design-vue';
 // import {registerGlobComp} from "/@/components/registerGlobComp";
 // registerGlobComp([Form,Row])
 interface FormState {
@@ -44,7 +48,10 @@ interface FormState {
 
 export default defineComponent({
   name: 'BasicForm',
-  components: {Form, Row},
+  components: {
+    UserOutlined,
+    LockOutlined,
+  },
   setup() {
     const formState: UnwrapRef<FormState> = reactive({
       user: '',
@@ -64,10 +71,6 @@ export default defineComponent({
       handleFinish,
       handleFinishFailed,
     };
-  },
-  components: {
-    UserOutlined,
-    LockOutlined,
   },
 });
 </script>
