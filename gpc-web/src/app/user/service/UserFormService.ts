43e016a396fde13450b5bb7736bcca981082bb6c
jac <devjac@ogarnij.se>
2021-09-23 21:52:04 +0200

    mija-1 load form in service not in view

--- gpc-web/src/app/user/service/UserFormService.ts
+++ gpc-web/src/app/user/service/UserFormService.ts
@@ -1,4 +1,4 @@
-import {reactive, UnwrapRef} from "vue";
+import {reactive, UnwrapRef, watchEffect} from "vue";
 import ajax from "/@/utils/service/ajax/ajax";
 import {FormUtilDto} from "/@/app/utils/formUtil/dto/FormUtilDto";
 import {FormState} from "/@/app/user/dto";
@@ -13,16 +13,26 @@ function loadDateForm(formUtil: UnwrapRef<FormUtilDto<FormState>>, id: number | 
             );
         };
         handleSearch(id);
+    } else {
+        formUtil.loading= false;
     }
 }
 
-export function userFormService(id: number | undefined) {
-    const formUtil: UnwrapRef<FormUtilDto<FormState>> = reactive({
+function initVal() {
+    return reactive({
         loading: true,
         formState: {}
     } as FormUtilDto<FormState>);
+}
 
-    loadDateForm(formUtil, id)
+export function userFormService(id: number | undefined) {
+    const formUtil: UnwrapRef<FormUtilDto<FormState>> = initVal();
+
+
+    watchEffect(() => {
+        formUtil.loading = true;
+        loadDateForm(formUtil, id)
+    });
 
     return {formUtil}
 }
