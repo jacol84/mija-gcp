2b0fe6f5c9aafa9b7e02c6ce9a8e74df2b4bbb18
jac <devjac@ogarnij.se>
2021-10-15 21:34:05 +0200

    mija-1 vite not working

--- gpc-web/vite.config.ts
+++ gpc-web/vite.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
         alias: [
             {
                 find: '/@',
-                replacement: path.resolve(__dirname,'/src')
+                replacement: path.resolve(__dirname, '/src')+"/"
             }
         ]
     },
