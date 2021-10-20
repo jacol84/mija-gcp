568eedb1ac98633caf1e08b9333ef7785622aa21
jac <devjac@ogarnij.se>
2021-10-20 21:12:05 +0200

    mija-1 FIX vite.config.ts

--- gpc-web/vite.config.ts
+++ gpc-web/vite.config.ts
@@ -1,14 +1,7 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-// import createImportPlugin from 'vite-plugin-import';
-// import antdImportPlugin from '@vitejs/plugin-antd-import';
-// import antdViteImportPlugin from 'antd-vite-import-plugin';
+import {resolve} from "path";
 
-const path = require('path')
-
-// import ViteComponents, {AntDesignVueResolver} from 'vite-plugin-components'
-
-// https://vitejs.dev/config/
 export default defineConfig({
     build: {
         // generate manifest.json in outDir
@@ -31,24 +24,18 @@ export default defineConfig({
         },
     },
     resolve: {
-        alias: {
-            '/@': path.resolve(__dirname, './src')
-        }
+        alias: [
+            {
+                find: /\/@\//,
+                replacement: resolve(process.cwd(), '.', 'src') + '/',
+            },
+        ]
     },
     plugins: [
         vue(),
-        // createImportPlugin([
-        //     {
-        //         libraryName: 'antd',
-        //         'style': true,   // or 'css'
-        //     }
-        // ]),
-        // antdViteImportPlugin(),
     ],
     optimizeDeps: {
-        // @iconify/iconify: The dependency is dynamically and virtually loaded by @purge-icons/generated, so it needs to be specified explicitly
         include: [
-            // '@iconify/iconify',
             'ant-design-vue/es/locale/pl_PL',
             'moment/dist/locale/pl',
             'ant-design-vue/es/locale/en_US',
